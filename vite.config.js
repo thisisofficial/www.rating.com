dbde49ec328956ab583475d45f4d4493dbbc72a1
thisisofficial <thisisofficiallyofficial@gmail.com>
2022-05-29 23:34:06 -0500

    Vite Config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://github.com/thisisofficial/www.rating.com
 export default defineConfig({
+  base:"/www.rating.com/",
   plugins: [react()]
 })
